626ab2634d9dcbc714d2e4a1768f2b6c5b5ea7c2
Jenna Gain <gain.jenna@gmail.com>
2016-05-10 09:33:32 -0700

    Initial Commit

---
+++ YP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YP
+//
+//  Created by Jenna Gain on 5/10/16.
+//  Copyright (c) 2016 Jenna Gain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YP/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  YP
+//
+//  Created by Jenna Gain on 5/10/16.
+//  Copyright (c) 2016 Jenna Gain. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
